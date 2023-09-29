57a71a5890126911cc4963e5db3c793e3941adee
unknown <ashvvvaaa123@gmail.com>
2023-09-28 21:45:47 -0700

    clacyt talt mara

--- JS/Main_page.js
+++ JS/Main_page.js
@@ -2,11 +2,13 @@ let all_buttons = document.querySelectorAll(".button_a");
 let the_EX = document.getElementsByClassName("the_EX")[0];
 let delet_AC = document.getElementsByClassName("the_main_button2_1")[0];
 let delet_backspace = document.getElementsByClassName("the_main_button2_2")[0];
+let theFinelOutPut = document.getElementsByClassName("out_put")[0];
 all_buttons.forEach((ele) => {
     ele.addEventListener("click", () => {
         the_EX.append(ele.textContent)
         delet_AC.addEventListener("click",() =>{
             the_EX.textContent = null;
+            theFinelOutPut.textContent = null;
         })
     })
 });
@@ -16,7 +18,6 @@ delet_backspace.addEventListener("click",() => {
 // Oprator
 let Oprator = document.querySelectorAll(".Oprator");
 let Equal = document.getElementsByClassName("equal")[0];
-let theFinelOutPut = document.getElementsByClassName("out_put")[0];
 Equal.addEventListener("click" , () =>{
         let TheFirstPart = parseFloat(the_EX.textContent.slice(0,the_EX.textContent.indexOf("+")));
         let TheSecondPart = parseFloat(the_EX.textContent.slice(the_EX.textContent.indexOf("+") + 1,the_EX.textContent.length))
@@ -25,13 +26,20 @@ Equal.addEventListener("click" , () =>{
         if (the_EX.textContent.includes("+")){
             theFinelOutPut.textContent = TheFirstPart + TheSecondPart;
         }else if (the_EX.textContent.includes("-")){    
-            theFinelOutPut.textContent = TheFirstPart - parseFloat(the_EX.textContent.slice(the_EX.textContent.lastIndexOf("+"),the_EX.textContent.length));
+            theFinelOutPut.textContent = parseFloat(the_EX.textContent.slice(0,the_EX.textContent.indexOf("-"))) - parseFloat(the_EX.textContent.slice(the_EX.textContent.indexOf("-") + 1,the_EX.textContent.length));
         }else if (the_EX.textContent.includes("/")){
-            theFinelOutPut.textContent = TheFirstPart / parseFloat(the_EX.textContent.slice(the_EX.textContent.lastIndexOf("+"),the_EX.textContent.length)) ;
+
+
+
+            theFinelOutPut.textContent = parseFloat(the_EX.textContent.slice(0,the_EX.textContent.indexOf("/"))) / parseFloat(the_EX.textContent.slice(the_EX.textContent.indexOf("/")   + 1,the_EX.textContent.length))  ;
+        
+        
+        
+        
         }else if (the_EX.textContent.includes("*")){
-            theFinelOutPut.textContent = TheFirstPart * TheSecondPart;
+            theFinelOutPut.textContent = parseFloat(the_EX.textContent.slice(0,the_EX.textContent.indexOf("*"))) * parseFloat(the_EX.textContent.slice(the_EX.textContent.indexOf("*") + 1,the_EX.textContent.length));
         }else if (the_EX.textContent.includes("%")){
-            theFinelOutPut.textContent = TheFirstPart % parseFloat(the_EX.textContent.slice(the_EX.textContent.lastIndexOf("+"),the_EX.textContent.length));
+            theFinelOutPut.textContent = TheFirstPart % parseFloat(the_EX.textContent.slice(the_EX.textContent.lastIndexOf("%"),the_EX.textContent.length));
         }else {
             theFinelOutPut.textContent = "ERR (: 27sn   "
         }
